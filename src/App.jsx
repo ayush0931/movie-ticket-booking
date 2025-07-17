40c5e16ebe2a58593f01ac4f7075cb58fb5b4419
ayush <ayushkumarpandey.96@gmail.com>
2025-07-17 17:17:48 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Navbar from './component/Navbar'
+import {Route, Routes, useLocation} from 'react-router-dom'
+import Home from './pages/Home'
+import Favorite from './pages/Favorite'
+import Movie from './pages/Movie'
+import MovieDetail from './pages/MovieDetail'
+import SeatLayout from './pages/SeatLayout'
+import MyBookings from './pages/MyBookings'
+import Footer from './component/footer'
+import {Toaster} from 'react-hot-toast'
+
+const App = () => {
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
+  return (
+    <>
+    {Toaster}
+      {!isAdminRoute && <Navbar/>}
+       <Routes>
+      <Route path='/'  element={<Home/>} />
+       <Route path='/seatLayout'  element={<SeatLayout/>} />
+      <Route path='/favorite'  element={<Favorite/>} />
+      <Route path='/movies'  element={<Movie/>} />
+      <Route path='/movieDetail'  element={<MovieDetail/>} />
+      <Route path='/mybooking'  element={<MyBookings/>} />
+      </Routes>
+       {!isAdminRoute && <Footer/>}
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import HeroSection from '../component/HeroSection'
+import FeaturedSection from '../component/FeaturedSection'
+
+
+const Home = () => {
+  return (
+    <div>
+      <HeroSection/>
+      <FeaturedSection/>
+      
+      
+    </div>
+  )
+}
+
+export default Home
